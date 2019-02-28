12f8b32486acaecb8f8a85707ba56deca3a4799c
zhangxulei <shitangdejiaozi@163.com>
2019-02-28 16:51:48 +0800

    添加vector和矩阵的基本结构

--- simple3d-master/simple3d-master/BasicMath.h
+++ simple3d-master/simple3d-master/BasicMath.h
@@ -3,10 +3,50 @@
 
 typedef unsigned int IUINT32;
 
-typedef struct
+//向量，顶点相关////////////////////////////////////////////////////
+typedef struct VECTOR3D_TYP
 {
-	float m[4][4];
-} matrix_t;
+	union
+	{
+		float M[3];
+		struct
+		{
+			float x, y, z;
+		};
+	};
+} VECTOR3D, *VECTOR3D_PTR;
+
+typedef struct VECTOR4D_TYP
+{
+	union
+	{
+		float M[4];
+		struct
+		{
+			float x, y, z, w;
+		};
+	};
+} VECTOR4D, POINT4D, *VECTOR4D_PTR;
+
+
+
+//矩阵相关 ////////////////////////////////////////////////////////
+typedef struct MATRIX4X4_TYP
+{
+	union
+	{
+		float m[4][4];
+
+		struct
+		{
+			float M00, M01, M02, M03;
+			float M10, M11, M12, M13;
+			float M20, M21, M22, M23;
+			float M30, M31, M32, M33;
+		};
+	};
+
+} MATRIX4X4;
 
 #endif // !BASICMATH
 
